3fcdbf11eb3807c89401c605eb64987c6901085f
chauhanrohit11 <rohitchauhan.mnnit@gmail.com>
2022-01-27 17:53:21 +0530

    added admin user test

--- app/core/models.py
+++ app/core/models.py
@@ -5,20 +5,20 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, \
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password):
+    def create_user(self, email, password, **extra_fields):
         """Creates and save a User with email and password"""
         if not email:
             raise ValueError('User must have an email address')
 
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password, **extra_fields):
         """Creates and saves a new superuser"""
-        user = self.create_user(email, password)
+        user = self.create_user(email, password, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
